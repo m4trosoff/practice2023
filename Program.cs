15b361c7d9990217a044a41233aa68bdff3d1995
m4trosoff <sobaka.ezhik@gmail.com>
2023-04-04 00:01:17 +0300

    Stasik creation

--- Program.cs
+++ Program.cs
@@ -6,10 +6,25 @@ using System.Threading.Tasks;
 
 namespace practiceup
 {
+    class Student
+    {
+        public string Name { get; set; }
+        public int Course { get; set; }
+        public bool Gender { get; set; }
+
+        public Student(string name, int course, bool gender)
+        {
+            Name = name;
+            Course = course;
+            Gender = gender;
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            Student stasik = new Student("Стасик", 3, false);
         }
     }
 }
